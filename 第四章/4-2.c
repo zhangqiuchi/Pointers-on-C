b6fdd38561a3982560b74624890769a79399f94c
zhangqiuchi <2078720351@qq.com>
2019-12-02 22:36:42 +0800

    4.2

--- 第四章/4-2.c
+++ 第四章/4-2.c
@@ -4,7 +4,7 @@
 int main()
 {
 	int i,j;
-	for (i = 2; i < 101; i ++)
+/*	for (i = 2; i < 101; i ++)
 	{
 		for (j = 2; j <i; j ++)
 			if (i % j == 0)
@@ -12,6 +12,34 @@ int main()
 		if (i == j)
 			printf("%d\n", i);
 	
-	}			
+	}*/
+
+/*	for (i = 1; i <= 100; i ++)
+	{	if (i == 1)
+			continue;
+		int k = sqrt(i);
+		for (j = 2; j <= k; j ++)
+		{
+			if (i % j == 0)
+			{	
+				break;
+			}
+		}
+		if (j > k)
+			printf("%d\n", i);
+	}*/
+/*	for (i = 1; i <= 100; i ++)
+	{
+		if (i == 1)
+			continue;
+		int k = i/2;
+		for (j = 2; j <= k; j ++)
+			if (i % j == 0)
+				break;
+		if (j > k)
+			printf("%d\n", i);
+	
+	}*/	
+
 	return 0;
 }
