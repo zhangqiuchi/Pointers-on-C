095e68dc9c5f33bf8f46451d2f7c24fda76c972a
zhangqiuchi <2078720351@qq.com>
2019-12-02 20:09:11 +0800

    4.4

--- 第四章/4-4.c
+++ 第四章/4-4.c
@@ -16,17 +16,32 @@ void copy_n(char dst[], char src[], int n)
 			dst[i] = '\0';
 	
 	}
-
-
-		
-	return;
+		return;
 }
 
-
+void copy(char dst[], char src[], int n)
+{
+	int num = 0;
+	
+	while (*src != '\0')
+	{
+		if (n == num)
+			break;
+		*dst++ = *src++;
+		num ++;
+	}
+	
+	if (num < n)
+		for (int i = 0; i < n - num; i ++)
+		
+			*(dst + i) = '\0';
+	return;
+}
 int main()
 {	char src[] = "zhangqiuchi";
 	char dat[100] = {'\0'};
-	copy_n(dat, src, 11);
+	copy(dat, src, 20);
 	printf("%s", dat);
+	system("pause");
 	return 0;
 }
